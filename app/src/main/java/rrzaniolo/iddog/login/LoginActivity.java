77b48ae79576379838fe68e4ccae04773dbe1fe5
Rodrigo Rodrigues Zaniolo <rrzaniolo@gmail.com>
2018-04-28 18:31:27 -0300

    LoginActivity
    WIP - Added basic structure to activity and ViewModel.

--- app/src/main/java/rrzaniolo/iddog/login/LoginActivity.java
+++ app/src/main/java/rrzaniolo/iddog/login/LoginActivity.java
@@ -1,6 +1,18 @@
 package rrzaniolo.iddog.login;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import rrzaniolo.iddog.R;
+import rrzaniolo.iddog.SnackbarMessage;
+import rrzaniolo.iddog.ViewModelFactory;
+import rrzaniolo.iddog.databinding.ActivityLoginBinding;
+import rrzaniolo.iddog.utils.SnackbarUtils;
+
+import static rrzaniolo.iddog.utils.Preconditions.checkNotNull;
 
 /**
  * Created by Rodrigo Rodrigues Zaniolo on 4/28/2018.
@@ -8,4 +20,62 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class LoginActivity extends AppCompatActivity {
+
+    //region --- Constants ---
+    private final String TAG = "LoginActivity";
+    //endregion
+
+    //region --- Variables ---
+    private LoginViewModel viewModel;
+    private ActivityLoginBinding binding;
+    //endregion
+
+    //region --- Getters and Setters ---
+    private LoginViewModel getViewModel() {
+        return viewModel;
+    }
+
+    private void setViewModel(LoginViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    private ActivityLoginBinding getBinding() {
+        return binding;
+    }
+
+    private void setBinding() {
+        this.binding = DataBindingUtil.setContentView(LoginActivity.this, R.layout.activity_login);
+    }
+    //endregion
+
+    //region --- Life Cycle ---
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setViewModel(ViewModelFactory.getInstance(getApplication()).create(LoginViewModel.class));
+        setBinding();
+        getBinding().setViewModel(getViewModel());
+        setUpSnackBar();
+    }
+    //endregion
+
+    //region --- Private Methods ---
+    private void setUpSnackBar(){
+        checkNotNull(getViewModel());
+
+        getViewModel().getSnackbarMessage().observe(LoginActivity.this, new SnackbarMessage.SnackbarObserver() {
+            @Override
+            public void onNewMessage(int snackbarMessageResourceId) {
+                try {
+                    checkNotNull(getBinding());
+                    checkNotNull(getBinding().getRoot());
+                    SnackbarUtils.showSnackbar(getBinding().getRoot(), getString(snackbarMessageResourceId));
+                }catch (NullPointerException e) {
+                    Log.e(TAG, e.getLocalizedMessage());
+                }
+            }
+        });
+    }
+    //endregion
 }
--- app/src/main/java/rrzaniolo/iddog/login/LoginViewModel.java
+++ app/src/main/java/rrzaniolo/iddog/login/LoginViewModel.java
@@ -3,6 +3,11 @@ package rrzaniolo.iddog.login;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
+import android.view.View;
+
+import rrzaniolo.iddog.R;
+import rrzaniolo.iddog.SnackbarMessage;
+import rrzaniolo.iddog.utils.SnackbarUtils;
 
 /**
  * Created by Rodrigo Rodrigues Zaniolo on 4/28/2018.
@@ -12,12 +17,40 @@ import android.support.annotation.NonNull;
 public class LoginViewModel extends AndroidViewModel {
 
     //region --- Variables ---
+    private final SnackbarMessage snackbarMessage = new SnackbarMessage();
+    //endregion
+
+    //region --- Getter and Setters ---
+    public SnackbarMessage getSnackbarMessage() {
+        return snackbarMessage;
+    }
     //endregion
 
     //region --- Constructors ---
-
     public LoginViewModel(@NonNull Application application) {
         super(application);
     }
     //endregion
+
+    //region -- Private Methods ---
+    private void showSnackbarMessage(Integer message) {
+        getSnackbarMessage().setValue(message);
+    }
+    //endregion
+
+    //region --- Public Methods ---
+    public View.OnClickListener onLogin(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        };
+    }
+    //endregion
+
+    //region --- API CALL ---
+    private void signIn (){
+        showSnackbarMessage(R.string.em_apiSignIn);
+    }
 }
